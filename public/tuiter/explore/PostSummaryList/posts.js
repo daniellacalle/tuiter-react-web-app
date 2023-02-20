6eeec190b71150f7970df5d8a8f92774037c4cee
Daniella Calle <daniellacalle@Daniellas-New-MacBook-Pro.local>
2023-02-19 20:26:39 -0500

    code working thank Gawd, updated an image for better visibility

--- public/tuiter/explore/PostSummaryList/posts.js
+++ public/tuiter/explore/PostSummaryList/posts.js
@@ -5,7 +5,7 @@ export default posts = [
                + "Single Page Applications or SPAs" },
     { topic: "", userName: "JavaScript", time: "1 day", image: "./images/js-logo.png", tweets : "123k",
         title: "JavaScript is programming language that can run on browsers as well as desktops" },
-    { topic: "Web Development", userName: "", time: "last week", image: "./images/jquery-logo.png",
+    { topic: "Web Development", userName: "", time: "last week", image: "./images/jq2-logo.png",
         tweets : "122k", title: "jQuery" },
     { topic: "Web Development", userName: "NodeJS", time: "last month", image: "./images/nodejs-logo.png",
         tweets : "1220k", title: "" },
