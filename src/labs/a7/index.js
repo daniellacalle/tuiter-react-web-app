efd15b3614622ade2e8a1d409ed2a175bded105c
Daniella Calle <daniella.calle@gmail.com>
2023-03-26 22:28:09 -0400

    added nav bar for a7 page

--- src/labs/a7/index.js
+++ src/labs/a7/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReduxExamples from "./redux-examples";
+import Nav from "../../nav";
 
 const Assignment7 = () => {
-    console.log("do we get here");
+
     return(
         <>
+            <Nav/>
             <h1>Assignment 7</h1>
             <ReduxExamples/>
         </>
